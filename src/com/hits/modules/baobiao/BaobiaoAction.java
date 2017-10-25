e7018f7a962dc4a1bfeaa0d32c4faf3a6c4133e5
jjflyx <1181869404@qq.com>
2017-10-25 22:54:13 +0800

    求盈利

--- src/com/hits/modules/baobiao/BaobiaoAction.java
+++ src/com/hits/modules/baobiao/BaobiaoAction.java
@@ -155,57 +155,38 @@ public class BaobiaoAction extends BaseAction {
 	}
 	@At
 	@Ok("->:/private/baobiao/zcfx.html")
-	public void zcfx(HttpSession session, HttpServletRequest req, @Param("startdate") String startdate,
-			 @Param("enddate") String enddate, @Param("type") String type){
+	public void zcfx(HttpSession session, HttpServletRequest req){
 		try {
 			Gson gson = new Gson();
-			enddate = EmptyUtils.isEmpty(enddate)?DateUtil.getToday():enddate;
-			startdate = EmptyUtils.isEmpty(startdate)?DateUtil.getFirstMonDay(enddate):startdate;
-			req.setAttribute("startdate",startdate);
 			req.setAttribute("year",DateUtil.getToday().substring(0, 4));
+			//当前年份的第一天
+			String firstday = DateUtil.getToday().substring(0, 5)+"01-01";
+			//当前年份的最后一天
+			String endday = DateUtil.getToday();
 			//得到月份
 			List<Sys_unit> xzqhList = daoCtl.list(dao,Sys_unit.class,Sqls.create(" select id,name from sys_unit where unittype = 88 order by id asc "));
 			req.setAttribute("xzqhList",xzqhList);
-			List<String> months = DateUtil.getMonthBetween(DateUtil.getToday().substring(0, 5)+"01-01", DateUtil.getToday().substring(0, 5)+"12-31");
+			List<String> months = DateUtil.getMonthBetween(firstday,endday);
 			req.setAttribute("months",months);
 			String a="select CONCAT(YEAR(fkrq),'-',DATE_FORMAT(fkrq,'%m')) months ,sum(sfjk) as "
 					+ "count_amount from l_hkzd WHERE fkrq BETWEEN '2017-01-02' AND '2017-12-30' group by months";
-			//图形报表Map
-			Map<String,String> xyNameMap = daoCtl.getHTable(dao,Sqls.create(" select code,name from cs_value where typeid = '00010005' and state = 0 and code < '0008' order by location asc"));
-			List<String> xyCode = daoCtl.getStrRowValues(dao,Sqls.create(" select code,name from cs_value where typeid = '00010005' and state = 0 and code < '0008' order by location asc"));
-			req.setAttribute("xyNameMap",gson.toJson(xyNameMap));
 			//开始处理报表数据
-			Map<String,String> allMap = new HashMap<String, String>();//订单数量
-			Map<String,String> wfkMap = new HashMap<String, String>();//未付款
-			Map<String,String> yfkMap = new HashMap<String, String>();//已付款
-			Map<String,String> zjMap = new HashMap<String, String>();//总计
-			//查询订货单位的订单数量
-			String sqlstr=" select unitid,count(*) from l_jsgg where add_time between '" + startdate + "' and '" + enddate + "' group by unitid";
-			allMap = daoCtl.getHTable(dao,Sqls.create(sqlstr));
-			//查询订货单位未付款信息
-			sqlstr = "select unitid ,sum(yfjk) from l_jsgg where isfh='0002' and add_time between '" + startdate + "' and '" + enddate + "' group by unitid";
-			wfkMap = daoCtl.getHTable(dao, Sqls.create(sqlstr));
-			//查询订货单位已付款信息
-			sqlstr = "select unitid ,sum(yfjk) from l_jsgg where isfh='0001' and add_time between '" + startdate + "' and '" + enddate + "' group by unitid";
-			yfkMap = daoCtl.getHTable(dao, Sqls.create(sqlstr));
-			//订货单位所有账目信息
-			sqlstr = "select unitid ,sum(yfjk) from l_jsgg where add_time between '" + startdate + "' and '" + enddate + "' group by unitid";
-			zjMap = daoCtl.getHTable(dao, Sqls.create(sqlstr));
+			Map<String,String> srMap = new HashMap<String, String>();
+			Map<String,String> zcMap = new HashMap<String, String>();
+			//查询月度支出
+			String sqlstr="select CONCAT(YEAR(fkrq),'-',DATE_FORMAT(fkrq,'%m')) months ,sum(sfjk) as "
+					+ "count_amount from l_hkzd WHERE fkrq BETWEEN '"+firstday+"' AND '"+endday+"' group by months";
+			zcMap = daoCtl.getHTable(dao,Sqls.create(sqlstr));
+			//查询月度收入
+			sqlstr="select CONCAT(YEAR(add_time),'-',DATE_FORMAT(add_time,'%m')) months ,sum(yfjk) as "
+					+ "count_amount from l_jsgg WHERE add_time BETWEEN '"+firstday+"' AND '"+endday+"' group by months";
+			srMap = daoCtl.getHTable(dao, Sqls.create(sqlstr));
 			JSONArray array = new JSONArray();
-			for(Sys_unit unit : xzqhList){
-				JSONObject jsonroot = new JSONObject();
-				jsonroot.put("id", unit.getId());
-				jsonroot.put("name",unit.getName());
-			}
 			req.setAttribute("charsData", array.toString());
-			req.setAttribute("allMap",allMap);
-			req.setAttribute("wfkMap",wfkMap);
-			req.setAttribute("yfkMap",yfkMap);
-			req.setAttribute("zjMap",zjMap);
-			req.setAttribute("ddzs", daoCtl.getStrRowValue(dao, Sqls.create("select count(*) from l_jsgg where add_time between '" + startdate + "' and '" + enddate + "'")));
-			req.setAttribute("wfkzs", daoCtl.getStrRowValue(dao, Sqls.create("select sum(yfjk) from l_jsgg where isfh='0002' and add_time between '" + startdate + "' and '" + enddate + "'")));
-			req.setAttribute("yfkzs", daoCtl.getStrRowValue(dao, Sqls.create("select sum(yfjk) from l_jsgg where isfh='0001' and add_time between '" + startdate + "' and '" + enddate + "'")));
-			req.setAttribute("jkzj", daoCtl.getStrRowValue(dao, Sqls.create("select sum(yfjk) from l_jsgg where add_time between '" + startdate + "' and '" + enddate + "'")));
+			req.setAttribute("srMap",srMap);
+			req.setAttribute("zcMap",zcMap);
+			req.setAttribute("srzj", daoCtl.getStrRowValue(dao, Sqls.create("select sum(yfjk) from l_jsgg where add_time between '" + firstday + "' and '" + endday + "'")));
+			req.setAttribute("zczj", daoCtl.getStrRowValue(dao, Sqls.create("select sum(sfjk) from l_hkzd where fkrq between '" + firstday + "' and '" + endday + "'")));
 		}catch (Exception e){
 			e.printStackTrace();
 		}
